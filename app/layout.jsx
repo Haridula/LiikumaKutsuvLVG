3a6319dbe9ec3bea1cf2be1a940d5c4fe0e0da0a
Harid <haridula@proton.me>
2024-12-28 01:09:43 +0200

    Still trying

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
-import Nav from '../components/nav';
-import Loader from '../components/loader';
+import '@styles/globals.css';
+import Nav from '@components/nav';
+import Loader from '@components/loader';
 
 export const metadata = {
     title: "Liikuma Kutsuv LVG",
