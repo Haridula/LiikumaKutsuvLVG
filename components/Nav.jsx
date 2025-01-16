056e2e88a24ec6728755299953b3e00c7266e5cd
Harid <haridula@proton.me>
2025-01-16 19:05:34 +0200

    Fix silly aboutPage function in Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -15,6 +15,8 @@ export default function Nav() {
 
   const aboutPage = (lang) => { //Change language function only for navbar and only for about.
     setCurrentLanguage(lang);
+    if (lang === '/') router.push(`/about`);
+    else
     router.push(`/${lang}/about`);
   };
 
