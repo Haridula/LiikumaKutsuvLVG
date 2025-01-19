22f4efbcb5a84ac39b58c2d5fd5c45946886eb7c
Harid <haridula@proton.me>
2025-01-19 11:12:05 +0200

    Mobile devices adjustment

--- app/en/tracks/B/page.jsx
+++ app/en/tracks/B/page.jsx
@@ -5,105 +5,113 @@ import Image from 'next/image';
 
 const B = () => {
   return (
-    <div className='md:mx-64 md:px-36'>
+    <div className='mx-4 px-8'>
+      <div className='md:mx-64'>
 
-      {/* Desktop */}
+        {/* Desktop */}
 
-      <div className='max-sm:hidden'>
-        <div>
-          <h1 className='mt-2.5 mb-2.5 text-xl font-bold blue_gradient'>
-            B-wing:
-          </h1>
-        </div>
-        <hr className='mt-5 mb-5'/>
-        <div>
-          <div className='grid grid-cols-1 gap-6'>
-            <h2 className='mt-2.5 mb-2.5 font-bold text-base text-black'>
-              Placeholder h2
-            </h2>
-            <Image
-              className=""
-              src="/assets/images/logo.jpg"
-              alt="LVG Logo"
-              width={55}
-              height={55}
-            />
+        <div className='max-sm:hidden max-md:hidden'>
+          <div>
+            <h1 className='mt-2.5 mb-2.5 text-xl font-bold blue_gradient'>
+              A-hoone:
+            </h1>
           </div>
-          <p className='mt-5 mb-28 text-lg text-black'>
-            Placeholder paragraph
-          </p>
-        </div>
-
-        <hr className='mt-5 mb-5'/>
-
-        <div>
-          <div className='grid grid-cols-1 gap-6'>
-            <h2 className='mt-2.5 mb-2.5 font-bold text-base text-black'>
-              Placeholder h2
-            </h2>
-            <Image
-              className=""
-              src="/assets/images/logo.jpg"
-              alt="LVG Logo"
-              width={55}
-              height={55}
-            />
-          </div>
-            <p className='mb-28 text-lg text-black'>
-              Placeholder paragraph
-            </p>
-        </div>
-      </div>
-
-      {/* Mobile */}
-      
-      <div className='md:hidden'>
-        <div>
-          <h1 className='mt-2.5 mb-2.5 text-xl font-bold blue_gradient'>
-            B-wing:
-          </h1>
-        </div>
-        <hr className='mt-5 mb-5'/>
-        <div>
-          <div className='grid grid-cols-1 gap-6'>
-            <h2 className='mt-2.5 mb-2.5 font-bold text-base text-black'>
-              Placeholder h2
-            </h2>
+          <hr className='mt-5 mb-5'/>
+          <div>
+            <div className='grid grid-cols-2 gap-6'>
+              <h2 className='mt-2.5 mb-2.5 font-bold text-base text-black'>
+                Placeholder h2
+              </h2>
+              <Image
+                className=""
+                src="/assets/images/logo.jpg"
+                alt="LVG Logo"
+                width={55}
+                height={55}
+              />
+            </div>
             <div>
-              <p className='mb-12 text-lg text-black'>
+              <p className='mt-5 mb-28 text-lg text-black'>
                 Placeholder paragraph
               </p>
             </div>
+          </div>
 
+            <hr className='mt-5 mb-5'/>
+
+          <div>
+            <div className='grid grid-cols-2 gap-6'>
+              <h2 className='mt-2.5 mb-2.5 font-bold text-base text-black'>
+              Placeholder h2
+            </h2>
             <Image
-              className="mb-5"
+              className=""
               src="/assets/images/logo.jpg"
               alt="LVG Logo"
               width={55}
               height={55}
             />
+          </div>
+            <div>
+              <p className='mb-28 text-lg text-black'>
+                Placeholder paragraph
+              </p>
+            </div>
           </div>
         </div>
 
-        <hr className='mt-5 mb-10'/>
-        
-        <div>
-          <div className='grid grid-cols-1 gap-6'>
-            <h2 className='mt-2.5 mb-2.5 font-bold text-base text-black'>
-              Placeholder h2
-            </h2>
+        {/* Mobile */}
+      
+        <div className='md:hidden'>
           <div>
-            <p className='mb-12 text-lg text-black'>
-              Placeholder paragraph
-            </p>
+            <h1 className='mt-2.5 mb-2.5 text-xl font-bold blue_gradient'>
+              A-hoone:
+            </h1>
           </div>
-            <Image
-              className="mb-5"
-              src="/assets/images/logo.jpg"
-              alt="LVG Logo"
-              width={55}
-              height={55}
-            />
+
+          <hr className='mt-5 mb-5'/>
+
+          <div>
+            <div className='grid grid-cols-1 gap-6'>
+              <h2 className='mt-2.5 mb-2.5 font-bold text-base text-black'>
+                Placeholder h2
+              </h2>
+              <div>
+                <p className='mb-12 text-lg text-black'>
+                  Placeholder paragraph
+                </p>
+              </div>
+
+              <Image
+                className="mb-5"
+                src="/assets/images/logo.jpg"
+                alt="LVG Logo"
+                width={55}
+                height={55}
+              />
+            </div>
+          </div>
+
+          <hr className='mt-5 mb-10'/>
+        
+          <div>
+            <div className='grid grid-cols-1 gap-6'>
+              <h2 className='mt-2.5 mb-2.5 font-bold text-base text-black'>
+                Placeholder h2
+              </h2>
+              <div>
+                <p className='mb-12 text-lg text-black'>
+                  Placeholder paragraph
+                </p>
+              </div>
+              <Image
+                className="mb-5"
+                src="/assets/images/logo.jpg"
+                alt="LVG Logo"
+                width={55}
+                height={55}
+              />
+            </div>
           </div>
         </div>
       </div>
